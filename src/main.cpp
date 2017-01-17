02f627d5735e3f2e5b82a4d296b355d5161d09d4
Andrei Vasilev <andreisvasilev@gmail.com>
2017-01-16 17:19:07 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "Screen.h"
-#include "Swarm.h"
 
 int main() {
 
@@ -8,29 +7,17 @@ int main() {
 
     while(!screen.quit_program()) {
 
-        // Draw Particles
-        int elapsed = SDL_GetTicks();
-        swarm.move(elapsed);
-
-        unsigned char red = static_cast<unsigned char>(   (sin(elapsed * 0.0001) + 1) * 128  );
-        unsigned char green = static_cast<unsigned char>( (sin(elapsed * 0.0002) + 1) * 128  );
-        unsigned char blue = static_cast<unsigned char>(  (sin(elapsed * 0.0003) + 1) * 128  );
-
-        const ps::Particle * const p_particles = swarm.get_particles();
-
-        for(int i = 0; i < ps::Swarm::N_PARTICLES; ++i) {
-            ps::Particle particle = p_particles[i];
-            int x = static_cast<int>((particle.m_x_cord + 1) * ps::Screen::SCREEN_WIDTH / 2);
-            int y = static_cast<int>(particle.m_y_cord * ps::Screen::SCREEN_WIDTH / 2 + ps::Screen::SCREEN_HEIGHT/2);
-
-            screen.set_pixel_color(x, y, red, green, blue);
-        }
-        
-        screen.box_blur();
-
+        // Load current particle swarm.
+        screen.load_swarm(swarm);
 
         // Update SDL window with new particle positions and colors
         screen.update();
+
+        // Manipulate particle positions.
+        swarm.move(SDL_GetTicks());
+
+        // Apply gaussian blur effect.
+        screen.box_blur();
     }
 
     return 0;
